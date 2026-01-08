f8165ce0ad0e071e53b07bc5559f8d3479735c9d
dev-davies <dvsolaniyi@gmail.com>
2026-01-08 16:10:38 +0100

    header scroll edit

--- static/js/main.js
+++ static/js/main.js
@@ -177,7 +177,7 @@
 
         // Typed.js Initialization
         if ($('.typed-text-output').length == 1) {
-            var typed_strings = "NOVELSOLAR ACADEMY!, The Future of Energy!, Innovation & Sustainability!";
+            var typed_strings = "NOVELSOLAR ACADEMY!, The Future of Energy!, Innovation!, Sustainability!";
             var typed = new Typed('.typed-text-output', {
                 strings: typed_strings.split(', '),
                 typeSpeed: 100,
